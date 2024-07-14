831afe62711fbe08036627e19ccfe6f270d049ad
aryan-vaghasiya <103418372+aryan-vaghasiya@users.noreply.github.com>
2024-07-14 08:37:35 +0000

    learning dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "aryan@goodle.com"
+var accountPassword
+accountCity = "Surat"
+
+console.log(accountPassword);
+console.table([accountId, accountEmail, accountPassword])
